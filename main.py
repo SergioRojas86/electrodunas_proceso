0e220fec9d2e65cb290b56144579396de7ef857f
SergioRojas86 <rojas.sergio86@hotmail.com>
2024-05-06 20:31:11 -0500

    adding functions

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import sys
 import json
-import src.utils.logger_config
+from src.utils.logger_config import configure_logger, upload_to_s3
+import boto3
+
+# Configura el logger
+log_file_name = f'ejecución_{current_date}.log'
+logger = configure_logger(log_file_name)
 
 def main():
     json_arg = sys.argv[1]
     data = json.loads(json_arg)
     
     files_to_execute = data['files_to_execute']
-    logger_config.logger.info(f'Se realizara el procesamiento de las fechas: {files_to_execute}')
+    logger.info(f'Se realizara el procesamiento de las fechas: {files_to_execute}')
     
     
 if __name__ == "__main__":
+    s3_client = boto3.client('s3')
     main()
+    bucket_name_log = 'electrodunas-log-files'
+    upload_to_s3(log_file_name, bucket_name_log)
--- src/utils/logger_config.py
+++ src/utils/logger_config.py
@@ -1,10 +1,22 @@
 import logging
 import datetime
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-current_date = datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S")  # Obtener la fecha actual y formatearla
-file_handler = logging.FileHandler(f'registro_{current_date}.log')  # Utilizar la fecha en el nombre del archivo
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
+def configure_logger(log_file_name):
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    current_date = datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S")  # Obtener la fecha actual y formatearla
+    file_handler = logging.FileHandler(log_file_name)  # Utilizar la fecha en el nombre del archivo
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    return logger
+
+def upload_to_s3(log_file_name, bucket_name_log):
+    folder_name = 'execution-log'
+    s3 = boto3.client('s3')
+    object_name = os.path.join(folder_name, os.path.basename(log_file_name))
+    
+    s3.upload_file(log_file_name, bucket_name_log, object_name)
+    print(f"El archivo {log_file_name} ha sido subido exitosamente al bucket {bucket_name_log}.")
