93984a6a40b3b7b1759807a81021a5fa4c1f7153
SergioRojas86 <rojas.sergio86@hotmail.com>
2024-05-10 21:15:12 -0500

    Adding parameters

--- main.py
+++ main.py
@@ -19,17 +19,25 @@ def main(log_file, bucket_name_log, files_to_execute, s3_client):
     
 if __name__ == "__main__":
     s3_client = boto3.client('s3')
-    bucket_name_log = 'electrodunas-log-files'
-    log_file = "log_executed_files.csv"
+    #bucket_name_log = 'electrodunas-log-files'
+    #log_file = "log_executed_files.csv"
     
     json_arg = sys.argv[1]
     data = json.loads(json_arg)
+    log_bucket = data['log_bucket']
+    log_file = data['log_file']
+    cleaning_bucket = data['cleaning_bucket']
+    clean_folder = data['clean_folder']
+    stage_folder = data['stage_folder']
     files_to_execute = data['files_to_execute']
+    
+    logger.info(f'Parametros de ejecución: log_bucket: {log_bucket} - log_file: {log_file} - cleaning_bucket: {cleaning_bucket} - clean_folder: {clean_folder} - stage_folder: {stage_folder}')
+    
     if files_to_execute:
         logger.info(f'Se realizara el procesamiento de las fechas: {files_to_execute}')
-        main(log_file, bucket_name_log, files_to_execute, s3_client)
-        upload_log_to_s3(log_file_name, bucket_name_log, s3_client, logger)
+        main(log_file, log_bucket, files_to_execute, s3_client)
+        upload_log_to_s3(log_file_name, log_bucket, s3_client, logger)
     else:
         logger.info(f'No hay fechas para procesar')
-        upload_log_to_s3(log_file_name, bucket_name_log, s3_client, logger)
+        upload_log_to_s3(log_file_name, log_bucket, s3_client, logger)
     
