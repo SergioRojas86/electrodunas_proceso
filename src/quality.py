f12baf2b4428624c2c2e8479125f9f9565db1200
SergioRojas86 <rojas.sergio86@hotmail.com>
2024-05-12 17:13:42 -0500

    test

--- src/quality.py
+++ src/quality.py
@@ -1,6 +1,18 @@
-import csv
+import boto3
+import pandas as pd
+from io import BytesIO
+
+def check_csv_columns(s3_client, bucket, key, required_columns):
+    response = s3_client.get_object(Bucket=bucket, Key=key)
+    data = response['Body'].read()
+    df = pd.read_csv(BytesIO(data))
+    return all(column in df.columns for column in required_columns)
 
 def columns_to_use(s3_client, files_to_execute, cleaning_bucket, clean_folder):
+    
+    # Columnas requeridas
+    csv_columns = ['Active_energy', 'Reactive_energy', 'Voltaje_FA', 'Voltaje_FC']
+    xlsx_columns = ['Cliente:', 'Sector Económico:']
     
     files_to_check = [f"{clean_folder}/{item[0]}/" for item in files_to_execute]
     
@@ -9,4 +21,10 @@ def columns_to_use(s3_client, files_to_execute, cleaning_bucket, clean_folder):
     if 'Contents' in response:
         for obj in response['Contents']:
             key = obj['Key']
-            print(key)
+            # Verificar si el archivo está en uno de los directorios especificados
+            if any(key.startswith(directory) for directory in files_to_check):
+                if key.endswith('.csv'):
+                    if check_csv_columns(s3_client, cleaning_bucket, key, csv_columns):
+                        print(f"{key}: Contiene todas las columnas requeridas.")  
+                    else:
+                        print(f"{key}: NO contiene todas las columnas requeridas.")
