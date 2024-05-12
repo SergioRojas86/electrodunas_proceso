215955b73b30a6ca7b7d23b9c3146bc7bd79ac7c
SergioRojas86 <rojas.sergio86@hotmail.com>
2024-05-12 16:54:42 -0500

    test

--- src/quality.py
+++ src/quality.py
@@ -2,8 +2,7 @@ import csv
 
 def columns_to_use(s3_client, files_to_execute, cleaning_bucket, clean_folder):
     
-    
-    for file in files_to_execute:
-        print(file[0])
+    files_to_check = [f"{clean_folder}{item[0]}/" for item in files_to_execute]
+    print(files_to_check)
     #response = s3_client.list_objects_v2(Bucket=cleaning_bucket, Prefix=clean_folder+'/', Delimiter='/')
     #print(response)
