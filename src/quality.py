4c8b2bf2191338e6716a663ab139afd394a28831
SergioRojas86 <rojas.sergio86@hotmail.com>
2024-05-14 12:00:04 -0500

    test

--- src/quality.py
+++ src/quality.py
@@ -2,10 +2,7 @@ import boto3
 import pandas as pd
 from io import BytesIO
 
-def columns_to_use():
-    print(f'pandas: {pd.__version__}')
 
-'''
 # verificar completitud
 def completeness_csv(s3_client, logger, cleaning_bucket, key):
     # completitud
@@ -49,6 +46,8 @@ def check_xlsx_columns(s3_client, logger, cleaning_bucket, key, required_columns
 # revisra estructura y completitud de los archivos
 def columns_to_use(s3_client, logger, files_to_execute, cleaning_bucket, clean_folder):
     
+    print(f'pandas: {pd.__version__}')
+    
     # Columnas requeridas
     csv_columns = ['Active_energy', 'Reactive_energy', 'Voltaje_FA', 'Voltaje_FC']
     xlsx_columns = ['Cliente:', 'Sector Económico:']
@@ -78,6 +77,3 @@ def columns_to_use(s3_client, logger, files_to_execute, cleaning_bucket, clean_f
                         wrong_xlsx = 1
                         
     return files_with_missing_columns, wrong_xlsx
-        
-        
-'''
